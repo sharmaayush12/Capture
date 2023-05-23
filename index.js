0616302d9cd3f1c5296621d4dc2318b0c2b5e36b
sharmaayush12 <ayushsharma.wfa@gmail.com>
2023-05-23 18:11:22 +0530

    capute

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+var http = require('http');
+var fs = require('fs');
+var path = require('path')
+
+const app = express()
+
+app.use(function (res, req, next) {
+    res.header("Cross-Origin-Opener-Policy", "same-origin");
+    res.header("Cross-Origin-Embedder-Policy", "require-corp");
+    next()
+})
+
+http.createServer(function(req,res){
+    res.writeHead(200,{"Content-Type":"text/html"});
+    fs.createReadStream(path.resolve(__dirname,"index.html"))
+    .pipe(res)
+}).listen('3000');
+
+// app.get('/', (res, req) => {
+//     res.sendfile(__dirname + "/index.html")
+// })
+
+// app.listen(5000, () => {
+//     console.log("App is Listening on port 5000")
+// })
